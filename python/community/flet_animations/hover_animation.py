2aeb4660eec25a429283fbcffbedc682e9226ae9
non-npc <101848445+non-npc@users.noreply.github.com>
2024-09-12 12:00:36 -0400

    Update hover_animation.py
    Fixed exit method to ensure that the value of pb.value does not go below zero

--- python/community/flet_animations/hover_animation.py
+++ python/community/flet_animations/hover_animation.py
@@ -9,11 +9,11 @@ def main(page: ft.Page):
             pb.value+=0.01;
             page.update();
     def on_exit():
-        width=page.width;
         for i in range(100):
-            time.sleep(0.001);
-            pb.value-=0.01;
-            page.update();
+            time.sleep(0.001)
+            if pb.value > 0:
+                pb.value -= 0.01
+            page.update()
     page.horizontal_alignment = page.vertical_alignment = "center";
     progress_bar=pb = ft.ProgressBar(width=185, value=0, color="amber", bgcolor="#eeeeee");
     exp=ft.Column(alignment="center", controls=[ft.Row(alignment="center", controls=[ft.Column(controls=[ft.GestureDetector(
