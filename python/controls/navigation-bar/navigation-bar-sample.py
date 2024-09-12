413a81c1938f5fe2872ef2c090e1c62867f43ff1
non-npc <101848445+non-npc@users.noreply.github.com>
2024-09-12 12:55:09 -0400

    Update navigation-bar-sample.py
    replace the deprecated NavigationDestination with NavigationBarDestination

--- python/controls/navigation-bar/navigation-bar-sample.py
+++ python/controls/navigation-bar/navigation-bar-sample.py
@@ -1,14 +1,13 @@
 import flet as ft
 
-
 def main(page: ft.Page):
 
     page.title = "NavigationBar Example"
     page.navigation_bar = ft.NavigationBar(
         destinations=[
-            ft.NavigationDestination(icon=ft.icons.EXPLORE, label="Explore"),
-            ft.NavigationDestination(icon=ft.icons.COMMUTE, label="Commute"),
-            ft.NavigationDestination(
+            ft.NavigationBarDestination(icon=ft.icons.EXPLORE, label="Explore"),
+            ft.NavigationBarDestination(icon=ft.icons.COMMUTE, label="Commute"),
+            ft.NavigationBarDestination(
                 icon=ft.icons.BOOKMARK_BORDER,
                 selected_icon=ft.icons.BOOKMARK,
                 label="Explore",
@@ -16,6 +15,5 @@ def main(page: ft.Page):
         ]
     )
     page.add(ft.Text("Body!"))
-
 
 ft.app(target=main)
